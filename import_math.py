e4a46bf2a0b444a41e7973ea23513a033ec988fc
skwaweru254 <skwaweru254@gmail.com>
2022-12-09 15:38:14 +0300

    Initial commit

---
+++ import_math.py
@@ -0,0 +1,9 @@
+import math
+
+print(math.sin(60))  # sin of 60
+
+print(math.cos(60))  # cosine of 60
+
+print(math.factorial(4))
+
+print(math.floor(4.9))  # concatenate to the nearest whole number
